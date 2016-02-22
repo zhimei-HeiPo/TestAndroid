c5939121f379bb23fca0bbbb791d1626f4168bfa
HeiPo <zhangxb@outlook.com>
2016-02-22 14:43:45 +0800

    test add

--- app/src/main/java/com/test/medaiplayer/app/MainActivity.java
+++ app/src/main/java/com/test/medaiplayer/app/MainActivity.java
@@ -5,24 +5,29 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
     private MediaPlayerFragment mediaPlayerFragment;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayerFragment = new MediaPlayerFragment();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.fl_video, mediaPlayerFragment);
-        transaction.commit();
+        //mediaPlayerFragment = new MediaPlayerFragment();
+        //FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        //transaction.replace(R.id.fl_video, mediaPlayerFragment);
+        //transaction.commit();
+        textView = (TextView) findViewById(R.id.tv_content);
+        ((Button)findViewById(R.id.bt_click)).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        Log.d("","");
+        textView.setText("Hello HeiPo!");
     }
 }
---
+++ app/src/test/java/com/test/medaiplayer/app/MainActivityTest.java
@@ -0,0 +1,64 @@
+package com.test.medaiplayer.app;
+
+import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by HeiPo on 16/2/22.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+public class MainActivityTest {
+
+    // 引用待测Activity
+    private MainActivity mActivity;
+
+    // 引用待测Activity中的TextView和Button
+    private TextView textView;
+    private Button button;
+
+
+    @Before
+    public void setUp() throws Exception {
+        // 获取待测Activity
+        mActivity = Robolectric.setupActivity(MainActivity.class);
+
+        // 初始化textView和button
+        textView = (TextView) mActivity.findViewById(R.id.tv_content);
+        button = (Button) mActivity.findViewById(R.id.bt_click);
+    }
+
+
+    @Test
+    public void testInit() throws Exception {
+        assertNotNull(mActivity);
+        assertNotNull(textView);
+        assertNotNull(button);
+
+        assertEquals("com.test.medaiplayer.app", mActivity.getPackageName());
+
+        assertEquals("Hello world!", textView.getText().toString());
+    }
+
+    @Test
+    public void testButton() throws Exception {
+        button.performClick();
+
+        assertEquals("Hello HeiPo!", textView.getText().toString());
+    }
+
+    @Test
+    public void testFail() throws Exception {
+        fail("This case failed!");
+    }
+
+}
